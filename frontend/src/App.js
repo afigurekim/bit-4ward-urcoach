b641f176af0953c26525e0d1a6a4ce8f4616a578
afigurekim <afigurekim@gmail.com>
2019-07-12 16:53:19 +0900

    add Spring settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { 
+  Box, 
+  Button, 
+  Collapsible, 
+  Heading, 
+  Grommet, 
+  Layer, 
+  ResponsiveContext, 
+  Text
+} from 'grommet';
+import { FormClose, Notification } from 'grommet-icons';
+import logo from './assets/logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+  global: {
+    colors: {
+      brand: '#504c50'
+    },
+    font: {
+      family: 'Roboto, Noto Sans KR',
+      size: '14px',
+      height: '20px',
+    },
+  },
+}
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+class App extends Component {
+  state = {
+    showSidebar: false,
+  }
+  render() {
+    const { showSidebar } = this.state;
+    return (
+      <Grommet theme = { theme } full>
+        <ResponsiveContext.Consumer>
+          {size => (
+            <Box fill>
+              <AppBar>
+                <Heading level='3' margin='none'><img src={logo} alt="URCoach" style={{height: 30}} /></Heading>
+                <Button onClick={() => this.setState(prevState => ({ showSidebar: !prevState.showSidebar }))}>
+                  <Box pad="small" direction="row" align="center" gap="small">
+                    <Text>챌린지</Text>
+                  </Box>
+                </Button>
+                <Button hoverIndicator="dark-1" onClick={() => {}}>
+                  <Box pad="small" direction="row" align="center" gap="small">
+                    <Text>분석</Text>
+                  </Box>
+                </Button>
+                <Button hoverIndicator="dark-1" onClick={() => {}}>
+                  <Box pad="small" direction="row" align="center" gap="small">
+                    <Text>매칭</Text>
+                  </Box>
+                </Button>
+                <Button hoverIndicator="dark-1" onClick={() => {}}>
+                  <Box pad="small" direction="row" align="center" gap="small">
+                    <Text>채팅</Text>
+                  </Box>
+                </Button>
+                <Button hoverIndicator="dark-1" onClick={() => {}}>
+                  <Box pad="small" direction="row" align="center" gap="small">
+                    <Text>커뮤니티</Text>
+                  </Box>
+                </Button>
+                <Box direction="row" align="center" gap="small" pad="xsmall">
+                  <Button primary color="#7bb73d" label="로그인" onClick={() => {}} />
+                  <Button primary color="#fe7f2d" label="회원가입" onClick={() => {}} />
+                </Box>
+                <Button
+                  icon={<Notification />}
+                  onClick={() => this.setState(prevState => ({ showSidebar: !prevState.showSidebar }))}
+                />
+              </AppBar>
+              <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+                <Box flex align='center' justify='center'>
+                  App 바디
+                </Box>
+                {showSidebar &&(
+                  <Collapsible direction="vertical" open={showSidebar}>
+                    <Layer>
+                      <Box
+                        background='brand'
+                        tag='header'
+                        justify='end'
+                        align='center'
+                        direction='row'
+                      >
+                        <Button
+                          icon={<FormClose />}
+                          onClick={() => this.setState({ showSidebar: false })}
+                        />
+                      </Box>
+                      <Box
+                        fill
+                        background='brand'
+                        align='center'
+                        justify='center'
+                      >
+                        사이드bar
+                      </Box>
+                    </Layer>
+                  </Collapsible>
+                )}
+              </Box>
+            </Box>
+          )}
+        </ResponsiveContext.Consumer>
+      </Grommet>
+    );
+  }
 }
 
 export default App;
